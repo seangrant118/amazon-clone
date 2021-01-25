5992170aa13fb16e2a9d577d5548b754b2332d8b
Sean Grant <seangrant118@gmail.com>
2021-01-25 10:55:02 -0500

    home and product component

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Product from "./Product";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt="ex"
+        />
+      </div>
+      <div className="home__row">
+        <Product />
+      </div>
+      <div className="home__row"></div>
+      <div className="home__row"></div>
+    </div>
+  );
+}
+
+export default Home;
