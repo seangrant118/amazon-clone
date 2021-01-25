96ab9eb493a9fb3153bb7c847f25172faeaa134e
Sean Grant <seangrant118@gmail.com>
2021-01-25 11:28:26 -0500

    product props

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,12 +11,24 @@ function Home() {
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt="ex"
         />
+        <div className="home__row">
+          <Product
+            title="The Lean Startup"
+            price="29.99"
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+        </div>
       </div>
-      <div className="home__row">
-        <Product />
-      </div>
-      <div className="home__row"></div>
-      <div className="home__row"></div>
     </div>
   );
 }
