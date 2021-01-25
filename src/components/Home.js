6ac43582c549a620f13cbe25c8ca30302a2c84aa
Sean Grant <seangrant118@gmail.com>
2021-01-25 15:05:57 -0500

    dispatch add to basket

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,12 +13,14 @@ function Home() {
         />
         <div className="home__row">
           <Product
+            id={164743}
             title="The Lean Startup"
             price="29.99"
             image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
             rating={5}
           />
           <Product
+            id={938572}
             title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, DOugh Hook and Whisk, 5 Litre Class Bowl"
             price={239.0}
             rating={4}
@@ -27,18 +29,21 @@ function Home() {
         </div>
         <div className="home__row">
           <Product
+            id={337583}
             title="Brand New Fitbit Inspire HR Activity Tracker + Heart Rate - Black"
             price={199.99}
             rating={3}
             image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
           />
           <Product
+            id={123842}
             title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Light Fabric"
             price={98.99}
             rating={5}
             image="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSWzjKA650ZqBQ1o_k6TogH8PHPbv_Kjnfsod1Lq0pN8lYSVYQTVBaIwlmMG7WHcNnr4iykDv8cHnIk8sACf7WqdFgkz9KF"
           />
           <Product
+            id={850274}
             title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation"
             price={599.99}
             rating={4}
@@ -47,6 +52,7 @@ function Home() {
         </div>
         <div className="home__row">
           <Product
+            id={235761}
             title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Moniter"
             price={1099.99}
             rating={3}
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "../StateProvider";
 
-function Product({ title, image, price, rating }) {
+function Product({ title, image, price, rating, id }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    //dispatch action
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -21,7 +38,7 @@ function Product({ title, image, price, rating }) {
 
       <img src={image} alt={title} />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
