c72eb00f74d83417db7ac002408a4ab6c045f02c
Sean Grant <seangrant118@gmail.com>
2021-01-25 11:48:20 -0500

    homescreen product display

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,15 +18,40 @@ function Home() {
             image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
             rating={5}
           />
-          <Product />
+          <Product
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, DOugh Hook and Whisk, 5 Litre Class Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images.homedepot-static.com/productImages/8e9ac8a0-7e72-44bf-baae-e0f8229c01d3/svn/white-cuisinart-stand-mixers-sm-50-64_600.jpg"
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            title="Brand New Fitbit Inspire HR Activity Tracker + Heart Rate - Black"
+            price={199.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Light Fabric"
+            price={98.99}
+            rating={5}
+            image="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSWzjKA650ZqBQ1o_k6TogH8PHPbv_Kjnfsod1Lq0pN8lYSVYQTVBaIwlmMG7WHcNnr4iykDv8cHnIk8sACf7WqdFgkz9KF"
+          />
+          <Product
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation"
+            price={599.99}
+            rating={4}
+            image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-12-select-wifi-silver-202003_FMT_WHH?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1583551131102"
+          />
         </div>
         <div className="home__row">
-          <Product />
+          <Product
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Moniter"
+            price={1099.99}
+            rating={3}
+            image="https://cdn.shopify.com/s/files/1/0286/6960/8020/products/71_Qph9E_RL._AC_SL1500_1024x1024@2x.jpg?v=1594467906"
+          />
         </div>
       </div>
     </div>
